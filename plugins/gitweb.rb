4999ceca91dcf1a298e31b12c09a71fd3a9d1c2c
Pieter de Bie <pdebie@ai.rug.nl>
2008-07-06 21:43:06 +0200

    Add a configfile with multiple repositories

--- plugins/gitweb.rb
+++ plugins/gitweb.rb
@@ -1,8 +1,103 @@
-class Gitweb < PluginBase
+require 'net/http'
+require 'uri'
 
-  def hook_privmsg_chan(irc, msg)
-    if msg =~ /\s*[0-9a-f]{6,40}[\s,?-.]*/
-      irc.reply "Unable to look up sha $0"
+class GitwebLoader
+  
+  def initialize(configfile)
+    @config = File.open(configfile) { |f| YAML::load(f) }
+  end
+
+  def shorten(url)
+    http = Net::HTTP.start("tinyurl.com", 80)
+    response = http.post("/create.php", "url=#{url}")
+
+    if response.code == "200"
+      body = response.read_body
+      line = body.split("\n").find { |l| l =~ /hidden name=tinyurl/ }
+      i1 = line.index("http")
+      i2 = line.rindex("\"")
+      return line[i1...i2]
     end
   end
+  
+  def config
+    @config ||= File.open("repositories.yaml") { |f| YAML::load(f) }
+  end
+
+  def get_details(url, type, ref)
+    return {} unless type == "commit" || type == "tag"
+
+    a = Net::HTTP.get(URI.parse(url + "/?a=#{type};h=#{ref}"))
+    return { :subject => $1 } if a =~ /class=\"title".*?>(.*?)<(span|\/a)/
+
+    # Nothing found
+    return {}
+  end
+
+  def lookup_one(url, ref)
+    response = Net::HTTP.get_response(URI.parse(url + "/?a=object;h=#{ref}"))
+    if response.is_a? Net::HTTPRedirection
+      if response["Location"] =~ /\?a=(.*?)($|\&|;)/
+        type = $1
+        ret = { :type => type, :url => shorten(response["Location"]) }
+        ret = ret.merge(get_details(url, type, ref))
+        return ret
+      end
+    end
+    return nil
+  end
+
+  def lookup(irc, ref)
+    urls = config[irc.server.name][irc.channel.name] rescue []
+    urls.each do |url|
+      if a = lookup_one(url, ref)
+        return a
+      end
+    end
+    return nil
+  end
+
+end
+
+
+class Gitweb < PluginBase
+
+  def initialize(*args)
+    $config.merge(
+      'plugins' => {
+        :dir => true,
+        'gitweb' => {
+          :dir => true,
+          :help => 'Settings for the gitweb plugin.',
+          'configfile' => 'The file to read repositories from'
+        }
+      }
+    )
+    super(*args)
+  end
+
+  def load
+    @loader = GitwebLoader.new($config["plugins/gitweb/configfile"])
+  end
+
+  def try_lookup(irc, ref)
+    if r = @loader.lookup(irc, ref)
+      s = "#{ref[0..8]} is a #{r[:type]}. Gitweb: #{r[:url]}"
+      s << " -- #{r[:subject]}" if r[:subject]
+      irc.reply s
+      return true
+    end
+    return nil
+  end
+
+  def hook_privmsg_chan(irc, msg)
+    if msg =~ /\s*([0-9a-f]{6,40})\s*/
+      try_lookup(irc, $1)
+    elsif msg =~ /::(.*?)(\s|$|::)/
+      unless try_lookup(irc, $1)
+        irc.reply("I'm sorry, there's no such object #{$1}")
+      end
+    end
+  end
+
 end
