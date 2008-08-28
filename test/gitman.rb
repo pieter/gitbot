7dee7f1ce5d25c5d0206df46b3030e00d97d003d
Pieter de Bie <pdebie@ai.rug.nl>
2008-08-28 16:45:19 +0200

    Gitman: prettify output

--- test/gitman.rb
+++ test/gitman.rb
@@ -16,16 +16,16 @@ class GitManPluginTest < Test::Unit::TestCase
   def test_nil_irc_reply
     @web.hook_privmsg_chan(@irc, "I like man git-rev-parse")
     sleep(2)
-    assert_match(/\[git-rev-parse\]: http:\/\/.*/, @irc.message)
+    assert_match(/Pieter: the git-rev-parse manpage can be found at http:\/\/.*/, @irc.message)
   end
 
   def test_implicit_with_reply
     @web.hook_privmsg_chan(@irc, "John: see man git-log")
     sleep(3)
-    assert_match(/John: \[git-log\]: http.*$/, @irc.message)
+    assert_match(/John: the git-log manpage can be found at http.*$/, @irc.message)
     @web.hook_privmsg_chan(@irc, "Jane, see man git-diff")
     sleep(3)
-    assert_match(/Jane: \[git-diff\]: http.*$/, @irc.message)
+    assert_match(/Jane: the git-diff manpage can be found at http.*$/, @irc.message)
   end
 
 end
